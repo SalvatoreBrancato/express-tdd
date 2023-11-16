f5cc75e8d906cbc7e1a273102ee17c78f57945eb
SalvatoreBrancato <salvatorebrancato90@gmail.com>
2023-11-16 15:29:14 +0100

    sostituzione spazi

--- createSlug.js
+++ createSlug.js
@@ -1,5 +1,12 @@
 module.exports= function (stringa){
+    
+    //trasforma in stinga
     let slug = stringa.toString()
+    
+    //trasforma in minuscolo
     slug = slug.toLowerCase()
+
+    //sostituisci gli spazi con -
+    slug = slug.split(" ").join("-")
     return slug
 }
--- createSlug.test.js
+++ createSlug.test.js
@@ -15,3 +15,10 @@ test('createSlug dovrebbe ritornare una stringa in lowercase', ()=>{
     expect(result).toBe("minuscolo")
 })
 
+test('createSlug dovrebbe ritornare una stringa con gli spazi sostituiti da -', ()=>{
+    const stringa = "senza spazio"
+    const result = createSlug(stringa)
+
+    expect(result).toBe("senza-spazio")
+})
+
